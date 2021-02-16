408d35feb46eb210e495251478f154356e389aa1
Jerome van den Wyenbergh <jeyvdw@outlook.de>
2021-02-16 01:08:02 +0100

    Update-Abfrage hinzugefügt; Core auf 0.1.0-Pre.36 geupdatet; Noch nicht mit SQL funktionsfähig wegen Fehler im CorePlayer

--- common/src/main/java/dev/minecode/coins/common/CoinsCommon.java
+++ common/src/main/java/dev/minecode/coins/common/CoinsCommon.java
@@ -3,6 +3,7 @@ package dev.minecode.coins.common;
 import dev.minecode.coins.common.api.CoinsAPIProvider;
 import dev.minecode.coins.common.object.CoinsPlayerAddon;
 import dev.minecode.core.api.CoreAPI;
+import dev.minecode.core.api.manager.UpdateManager;
 
 import java.sql.SQLException;
 
@@ -15,6 +16,19 @@ public class CoinsCommon {
 
     public CoinsCommon() {
         makeInstances();
+
+        if (CoreAPI.getInstance().isUsingSQL()) {
+            try {
+                CoreAPI.getInstance().getDatabaseManager().getStatement().executeUpdate("CREATE TABLE IF NOT EXISTS minecode_coins (ID INT, COINS INT , PRIMARY KEY (ID))");
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+        UpdateManager updateManager = CoreAPI.getInstance().getUpdateManager();
+        if (updateManager.updateAvailable()) {
+            System.out.println("[" + CoreAPI.getInstance().getPluginName() + "] There is a newer Version available! You can download it at " + updateManager.getReleaseURL(updateManager.getMatchingRelease()));
+        }
     }
 
     public static CoinsCommon getInstance() {
@@ -24,14 +38,6 @@ public class CoinsCommon {
     private void makeInstances() {
         instance = this;
         coinsAPIProvider = new CoinsAPIProvider();
-
-        if (CoreAPI.getInstance().isUsingSQL()) {
-            try {
-                CoreAPI.getInstance().getDatabaseManager().getStatement().executeUpdate("CREATE TABLE IF NOT EXISTS minecode_coins (ID INT, COINS INT , PRIMARY KEY (ID))");
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-        }
     }
 
     public CoinsPlayerAddon getCoinsPlayerAddon() {
